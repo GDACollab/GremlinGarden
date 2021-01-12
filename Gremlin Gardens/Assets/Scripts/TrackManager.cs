327346b38edcbf1c09fc7f3fc296e71c5b0a90ed
ambiguousname <tyly04@gmail.com>
2021-01-11 17:24:25 -0800

    Imported Bezier Path, Started Work
    Imported the Bezier Path module (it should be fine to use) , Added an Example Scene, Started Working on the Classes the track system will be using.

---
+++ Gremlin Gardens/Assets/Scripts/TrackManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Will handle transferring a gremlin along a set of track modules. A track manager must be attached to an object with a bunch of child objects. All of those child objects
+/// must have the TrackManager and Path Creator component.
+/// </summary>
+public class TrackManager : MonoBehaviour
+{
+
+    public TrackModule activeModule;
+    void Start()
+    {
+        
+    }
+}
---
+++ Gremlin Gardens/Assets/Scripts/TrackModule.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Handles the individual aspects of each track module. Should be attached to an object with the PathCreator component. Will handle movement and animation along the track, if the appropriate
+/// attributes are provided.
+/// </summary>
+public class TrackModule : MonoBehaviour
+{
+    /// <summary>
+    /// Percentage of how fast the gremlin should be going in comparison to the BaseSpeed.
+    /// </summary>
+    public float Speed = 1.0f;
+    /// <summary>
+    /// 
+    /// </summary>
+    public string RelevantStat = "";
+    /// <summary>
+    /// How fast the Gremlin moves in units per fixed frame count.
+    /// </summary>
+    public float BaseSpeed = 5.0f;
+
+    public AnimationState
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
