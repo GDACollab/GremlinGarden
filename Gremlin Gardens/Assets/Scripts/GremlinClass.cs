2399c5354ee8dc2ff56cb5badf52b4f12011f223
jwdicker <73141644+jwdicker@users.noreply.github.com>
2021-01-13 18:28:48 -0800

    Gremlin stat framework
    Added the basic framework for setting, modifying, and retreving stats for gremlins.

--- Gremlin Gardens/Assets/Scripts/GremlinClass.cs
+++ Gremlin Gardens/Assets/Scripts/GremlinClass.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Class attatched to each Gremlin gameObject
 public class GremlinClass : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -14,5 +15,51 @@ public class GremlinClass : MonoBehaviour
     void Update()
     {
         
+    }
+}
+
+// Class that defines and alters each Gremlin's stats
+public class Gremlin : Object
+{
+    // Array of all stats associated with each gremlin
+    private float[] gremlinStats;
+
+    // Dictionary of all indices in gremlinStats with a key of what stat that index represents
+    public static readonly Dictionary<string, int> gremlinStatIndices = new Dictionary<string, int>()
+    {
+        {"Stamina", 0},
+        {"Happiness", 1},
+        {"Running", 2},
+        {"Climbing", 3},
+        {"Running", 4},
+        {"Swimming", 5},
+        {"Flying", 6}
+    };
+
+    public Gremlin()
+    {
+        gremlinStats = new float[gremlinStatIndices.Count];
+    }
+
+    /*
+     * Modifies the value of a specified stat to a given value
+     * 
+     * @param stat - The name of the stat 
+     * @param value - The value that stat will be set to
+     */
+    public void setStat(string stat, float value)
+    {
+        gremlinStats[gremlinStatIndices[stat]] = value;
+    }
+
+    /*
+     * Gives the value of a specified stat
+     * 
+     * @param stat - The name of the stat 
+     * @return - The value of the specified stat
+     */
+    public float getStat(string stat)
+    {
+        return gremlinStats[gremlinStatIndices[stat]];
     }
 }
