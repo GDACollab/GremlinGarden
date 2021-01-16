f43153601b79b6e8031a5eb13fe9eb9170105aa6
ambiguousname <tyly04@gmail.com>
2021-01-15 21:37:12 -0800

    More things with which to build tracks

---
+++ Gremlin Gardens/Assets/Scripts/Terrain Variants/SwimVariant.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwimVariant : TerrainVariant
+{
+    public override float relativeSpeed(Gremlin gremlin)
+    {
+        return base.relativeSpeed(gremlin);
+    }
+
+    public override Vector3 positionFunction(float time)
+    {
+        return new Vector3(0, Mathf.Sin(time) * 0.15f, 0);
+    }
+}
