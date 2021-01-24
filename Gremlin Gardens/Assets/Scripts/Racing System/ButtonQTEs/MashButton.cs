e7f777ec1bc76e7dae0963c021c36aae4d44de7f
ambiguousname <tyly04@gmail.com>
2021-01-24 13:11:43 -0800

    QTE animations!

--- Gremlin Gardens/Assets/Scripts/Racing System/ButtonQTEs/MashButton.cs
+++ Gremlin Gardens/Assets/Scripts/Racing System/ButtonQTEs/MashButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MashButton : QTEScript
 {
@@ -27,6 +28,23 @@ public class MashButton : QTEScript
     /// </summary>
     float speedChange;
 
+    /// <summary>
+    /// First sprite in the animation
+    /// </summary>
+    public Sprite firstSprite;
+    /// <summary>
+    /// Second sprite in the animation.
+    /// </summary>
+    public Sprite secondSprite;
+
+    /// <summary>
+    /// The frame rate of the animation.
+    /// </summary>
+    [Tooltip("The Frame Rate of the animation. Not really a frame rate, it's based on Time.deltaTime, but... whatever.")]
+    public float frameRate = 1;
+
+    float frames;
+
     [HideInInspector]
     public bool isKeyDown;
     // Start is called before the first frame update
@@ -34,6 +52,7 @@ public class MashButton : QTEScript
     {
         isKeyDown = false;
         speedChange = 0;
+        frames = 0;
     }
 
     public override float ModifySpeed() //Just increase the speed by however much we've calculated it.
@@ -55,6 +74,19 @@ public class MashButton : QTEScript
         if (Input.GetKeyUp(key) && isKeyDown == true)
         {
             isKeyDown = false;
+        }
+        frames += Time.deltaTime;
+        if (frames > frameRate)
+        {
+            frames = 0;
+            if (GetComponent<Image>().sprite == firstSprite)
+            {
+                GetComponent<Image>().sprite = secondSprite;
+            }
+            else
+            {
+                GetComponent<Image>().sprite = firstSprite;
+            }
         }
     }
 }
