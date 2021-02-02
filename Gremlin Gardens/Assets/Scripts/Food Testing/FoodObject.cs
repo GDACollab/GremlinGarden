4aa3027ee5e4a56a3b51abfa11cfb631ebdfaee9
Rohan Venkatapuram <sharpedo75@gmail.com>
2021-02-01 16:52:22 -0800

    Created FoodObject class that can be attached to a black game object and be turned into a food. Created Food Test scene where a test gremlin (just a green cube) walks up to an eats the Food

---
+++ Gremlin Gardens/Assets/Scripts/Food Testing/FoodObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodObject : MonoBehaviour
+{
+
+    // Attach this script to an empty game object and it will become a food scaled to a size of 1
+    public int foodID;
+    private Food food;
+    public GameObject foodModel;
+    private Rigidbody rgbd;
+    public float size;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        //Sets the Model
+        MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter objMesh = gameObject.AddComponent<MeshFilter>();
+        objMesh.mesh = foodModel.GetComponent<MeshFilter>().sharedMesh;
+        //Sets the Material
+        renderer.material = foodModel.GetComponent<MeshRenderer>().sharedMaterial;
+        //Adds Necessary Collision Components
+        gameObject.AddComponent<SphereCollider>();
+        rgbd = gameObject.AddComponent<Rigidbody>();
+        //Scales Size Down
+        scaleObjectSize(gameObject, size);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void scaleObjectSize(GameObject gameObject, float newSize)
+    {
+        float currentSize = gameObject.GetComponent<Renderer>().bounds.size.y;
+        Vector3 scale = gameObject.transform.localScale;
+        scale = newSize * scale / currentSize;
+        gameObject.transform.localScale = scale;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Gremlin"))
+        {
+            Debug.Log("Gremlin ate Food!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Gremlin Gardens/Assets/Scripts/Food Testing/TempGremlinMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempGremlinMove : MonoBehaviour
+{
+    //Makes the gremlin look for food an move towards it
+    // Start is called before the first frame update
+    private Rigidbody rigidbody;
+    public float speed;
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FoodObject[] foods = FindObjectsOfType<FoodObject>();
+        if (foods.Length != 0)
+        {
+            gameObject.transform.LookAt(foods[0].transform.position);
+            rigidbody.velocity = transform.forward * speed;
+        } else
+        {
+            rigidbody.velocity = Vector3.zero;
+        }
+    }
+}
