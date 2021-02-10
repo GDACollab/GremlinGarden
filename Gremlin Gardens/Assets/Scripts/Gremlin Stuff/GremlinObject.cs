448897b84dc4673f6a1918d05b343b422302fcc6
ambiguousname <tyly04@gmail.com>
2021-02-09 18:00:36 -0800

    Get racetracks working

--- Gremlin Gardens/Assets/Scripts/Gremlin Stuff/GremlinObject.cs
+++ Gremlin Gardens/Assets/Scripts/Gremlin Stuff/GremlinObject.cs
@@ -6,16 +6,23 @@ using UnityEngine;
 public class GremlinObject : MonoBehaviour
 {
     // Variables for the in-game gremlins
+    [HideInInspector]
     public Gremlin gremlin;
     Rigidbody body;
-    string gremlinName;
+    public string gremlinName;
     
     // Start is called before the first frame update
     void Start()
     {
         // Defines the Gremlin
         gremlin = new Gremlin(gremlinName);
-
+        //Temporary values to work with the track system.
+        gremlin.setStat("Running", 1);
+        gremlin.setStat("Flying", 1);
+        gremlin.setStat("Stamina", 1);
+        gremlin.setStat("Climbing", 1);
+        gremlin.setStat("Happiness", 1);
+        gremlin.setStat("Swiming", 1);
         //Adds Necessary Collision Components
         gameObject.AddComponent<SphereCollider>();
         body = gameObject.AddComponent<Rigidbody>();
