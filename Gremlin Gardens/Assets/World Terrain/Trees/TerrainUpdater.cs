10a2bee196ef55b1d3e523ec0ba6a398852274c3
Ray Nelson <rayolinels@gmail.com>
2021-02-22 17:24:28 -0800

    Path shader sharp edges, smaller hub

---
+++ Gremlin Gardens/Assets/World Terrain/Trees/TerrainUpdater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainUpdater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TerrainCollider>().enabled = false;
+        GetComponent<TerrainCollider>().enabled = true;
+
+        TerrainData terrainData = GetComponent<Terrain>().terrainData;
+
+        float[,] heights = terrainData.GetHeights(0, 0, 0, 0);
+        terrainData.SetHeights(0, 0, heights);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
