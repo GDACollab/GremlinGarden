3f8219ea5f18ca13934af996d1275b6a3bd1827f
ambiguousname <tyly04@gmail.com>
2021-03-01 19:04:32 -0800

    Added Scene Transitions, and Hub world can now transition to races

---
+++ Gremlin Gardens/Assets/Scripts/Scene Transitions/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// The class you use to load the next scene. Attach this to some object and use like a button or something to call the Load function.
+/// </summary>
+public class SceneLoader : MonoBehaviour
+{
+    public FadeManager fadeManager;
+    public void FadeOutLoad(string sceneName, float speed)
+    {
+        LoadingData.sceneToLoad = sceneName;
+        fadeManager.onFadeComplete.AddListener(StartLoadScene);
+        fadeManager.StartFade(speed, false);
+    }
+
+    /// <summary>
+    /// Meant to be used as a callback when the fade function is finished.
+    /// </summary>
+    void StartLoadScene() {
+        SceneManager.LoadScene("Loading");
+    }
+
+    public void Load(string sceneName) {
+        LoadingData.sceneToLoad = sceneName;
+        StartLoadScene(); //Just using this because it's easier than typing out the whole thing.
+    }
+}
