67af4c356f6e64b58f819d9d0755d5a0a183642e
jwdicker <73141644+jwdicker@users.noreply.github.com>
2021-03-01 21:01:18 -0800

    Shop Item Name Tag
    Replaced key input system with purchase on click and added moving nametags to each shop item

--- Gremlin Gardens/Assets/Scripts/FruitBuying.cs
+++ Gremlin Gardens/Assets/Scripts/FruitBuying.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class FruitBuying : MonoBehaviour
 {
-	public GameObject ItemPurchaseIndicator;  	  //button prompt to purchase item.
+	public static Dictionary<string, Vector3> TextPositions = new Dictionary<string, Vector3>();
+
+    /*public GameObject ItemPurchaseIndicator;  	  //button prompt to purchase item.
 	public GameObject ConfirmPurchaseIndicator;	  //button prompt to confirm purchase.
-    public GameObject CancelPurchaseIndicator;    //button prompt to drop down
+    public GameObject CancelPurchaseIndicator;    //button prompt to drop down*/
+    public GameObject PurchaseText; // Text that appears above the fruit
     public GameObject player;
 
+    public string name;
     private float distanceFromPlayer; //distance (in meters?) from player to fruit
     private bool onFruit; //is mouse currently over the fruit
     public bool purchaseIntent; //if player has pressed ItemPurchaseIndic key.
@@ -16,13 +20,20 @@ public class FruitBuying : MonoBehaviour
     public bool mouseOn;
 	//create a viewdFruit object
 
+    void Awake()
+    {
+        Vector3 selfPosition = this.transform.position;
+        TextPositions.Add(name, new Vector3(selfPosition.x, selfPosition.y + 0.4f, selfPosition.z + 0.3f));
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         mouseOn = false;
-        ItemPurchaseIndicator.SetActive(false);
+        PurchaseText.SetActive(false);
+        /*ItemPurchaseIndicator.SetActive(false);
         CancelPurchaseIndicator.SetActive(false);
-        ConfirmPurchaseIndicator.SetActive(false);
+        ConfirmPurchaseIndicator.SetActive(false);*/
     }
 
     // Update is called once per frame
@@ -31,7 +42,13 @@ public class FruitBuying : MonoBehaviour
     	distanceFromPlayer = Vector3.Distance(player.transform.position, this.transform.position);
         if (distanceFromPlayer < 20 && inShop && mouseOn)
         {
-            ItemPurchaseIndicator.SetActive(true);
+            centerText();
+            PurchaseText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Debug.Log($"Purchased {this.name}");
+            }
+            /*ItemPurchaseIndicator.SetActive(true);
             //freeze camera if input Purchase?
             if (Input.GetKeyDown("u"))
             {
@@ -55,13 +72,19 @@ public class FruitBuying : MonoBehaviour
                     CancelPurchaseIndicator.SetActive(false);
                     purchaseIntent = false;
                 }
-            }
+            }*/
         }
     }
 
     /* void FixedUpdate() {
     	transform.Rotate(0, 1, 0);
     }*/
+
+    // Centers PurchaseText over this
+    private void centerText()
+    {
+        PurchaseText.transform.position = TextPositions[this.name];
+    }
 
     // used for shopping as well
     private void OnMouseEnter()
@@ -73,8 +96,9 @@ public class FruitBuying : MonoBehaviour
     {
         mouseOn = false;
         purchaseIntent = false;
-        ItemPurchaseIndicator.SetActive(false);
+        PurchaseText.SetActive(false);
+        /*ItemPurchaseIndicator.SetActive(false);
         ConfirmPurchaseIndicator.SetActive(false);
-        CancelPurchaseIndicator.SetActive(false);
+        CancelPurchaseIndicator.SetActive(false);*/
     }
 }
