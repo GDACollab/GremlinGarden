6e0cdb32a04039009dd4b02be103320999c111f6
Ray Nelson <rayolinels@gmail.com>
2021-03-03 14:41:19 -0800

    Tutorial UI and scripts set up
    not fully implemented yet

---
+++ Gremlin Gardens/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TutorialManager : MonoBehaviour
+{
+    private List<TutorialTask> tasks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if a task is done?
+
+        // Update the UI list to check off completed tasks
+
+        // Bring up extended description of current item
+    }
+
+    // Called from each of the tutorial tasks to populate the list
+    public void AddTask() {
+
+    }
+
+    // Called from each of the tutorial tasks to check off its item in the list
+    public void CompletedTask() {
+
+    }
+}
---
+++ Gremlin Gardens/Assets/Scripts/Tutorial/TutorialTask.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Placed on each tutorializable thing, registers a UnityEvent with the TutorialManager
+/// </summary>
+public class TutorialTask : MonoBehaviour
+{
+    public UnityEvent addTask;
+    public UnityEvent completedTask;
+
+    // description text for each sub-task, such as "Press E to pet a gremlin"
+    public List<string> subtaskDescriptions;
+    public List<bool> subtaskCompletions;
+    
+    // description and completion status of the entire task
+    public string shortDescription;
+    public bool taskCompletion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (addTask != null) {
+            addTask.Invoke();
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && completedTask != null) {
+            completedTask.Invoke();
+        }
+    }
+}
