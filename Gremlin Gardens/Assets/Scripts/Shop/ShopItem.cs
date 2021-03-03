ba940e3acddbe582ea7ef92e25f69bf204836c83
ambiguousname <tyly04@gmail.com>
2021-03-02 18:02:39 -0800

    Started shop manager

---
+++ Gremlin Gardens/Assets/Scripts/Shop/ShopItem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItem : MonoBehaviour
+{
+
+    public string itemName;
+    public GameObject itemSpawnOnBuy;
+    private bool purchaseIntent; //if player has pressed ItemPurchaseIndic key.
+    private bool mouseOn;
+
+    private ShopManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouseOn = false;
+        manager = this.GetComponentInParent<ShopManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(manager.player.transform.position, this.transform.position) < 10 && mouseOn)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0) && purchaseIntent == false)
+            {
+                purchaseIntent = true;
+                manager.SetPurchaseText("Confirm Buy " + itemName + "?");
+            } else if (Input.GetKeyDown(KeyCode.Mouse0) && purchaseIntent == true) {
+                purchaseIntent = false;
+                var bought = Instantiate(itemSpawnOnBuy);
+                //Temporary solution for placement.
+                bought.transform.position = this.transform.position + new Vector3(0, 5, 0);
+                manager.SetPurchaseText("Buy " + itemName + "?");
+            }
+        }
+    }
+
+    // used for shopping as well
+    private void OnMouseEnter()
+    {
+        mouseOn = true;
+        manager.ItemHover(this);
+        manager.SetPurchaseText("Buy " + itemName + "?");
+    }
+
+    private void OnMouseExit()
+    {
+        manager.ItemExit();
+        mouseOn = false;
+        purchaseIntent = false;
+    }
+}
---
+++ Gremlin Gardens/Assets/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public List<GameObject> itemsToInstantiate;
+
+    [HideInInspector]
+    public List<ShopItem> shopItems;
+
+    public GameObject player;
+
+    public Vector3 itemStartingPos;
+
+    public Vector3 offsetDistance;
+
+    public GameObject PurchaseText;
+
+    Vector3 shopItemOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shopItemOffset = Vector3.zero;
+        for (int i = 0; i < itemsToInstantiate.Count; i++) {
+            AddItem(itemsToInstantiate[i]);
+        }
+    }
+
+    public GameObject AddItem(GameObject prefab) {
+        var newItem = Instantiate(prefab, this.transform);
+        newItem.transform.position = itemStartingPos + shopItemOffset;
+        shopItems.Add(newItem.GetComponent<ShopItem>());
+        shopItemOffset += offsetDistance;
+        return newItem;
+    }
+
+    public void RemoveItem(int index) {
+        for (int i = 0; i < shopItems.Count; i++)
+        {
+            if (i == index)
+            {
+                shopItems.Remove(shopItems[i]);
+                Destroy(shopItems[i].gameObject);
+            }
+        }
+    }
+    public void RemoveItem(GameObject itemToRemove) {
+        for (int i = 0; i < shopItems.Count; i++) {
+            if (shopItems[i].gameObject == itemToRemove) {
+                shopItems.Remove(shopItems[i]);
+                Destroy(shopItems[i].gameObject);
+            }
+        }
+    }
+
+    public void ItemHover(ShopItem item) {
+        PurchaseText.SetActive(true);
+        PurchaseText.transform.position = item.transform.position + new Vector3(0, 0.5f, 0);
+    }
+
+    public void SetPurchaseText(string text) {
+        PurchaseText.GetComponent<TextMesh>().text = text;
+    }
+
+    public void ItemExit() {
+        PurchaseText.SetActive(false);
+    }
+}
