9e6cc56e18683df937965516186d13da0487e579
rylee12 <ryanlee7x@gmail.com>
2021-04-29 01:38:02 -0700

    Update 1: Eat Ambient
    Implemented gremlin eating fruit on ground if it comes in contact. Fruit shrinks then disappears.

---
+++ Gremlin Gardens/Assets/Scripts/EatAmbient.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// To be attached to fruit
+public class EatAmbient : MonoBehaviour
+{
+    public float shrinkRate = 0.01f;
+    public float maxStatVal;
+
+    private Gremlin gremlin;
+    private FoodObject fruit;
+    private bool beingEaten = false;
+    // is beingCarried needed?
+    //private bool beingCarried = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fruit = this.GetComponent<FoodObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // needs the eating animation and eating sound
+        // add stat changes
+        //shrink food object     
+        if (beingEaten)
+        {
+            Vector3 scaleChange = new Vector3(shrinkRate, shrinkRate, shrinkRate);
+            this.gameObject.transform.localScale -= scaleChange * Time.deltaTime;
+
+            if (this.gameObject.transform.localScale.y < 0.00005f)
+            {
+                maxStatVal = gremlin.maxStatVal;
+                string stat = determineStat(fruit.foodName);
+
+                // don't go over max stat value
+                float statChange = gremlin.getStat(stat) + fruit.food.getStatAlteration(stat);
+                if (statChange > maxStatVal)
+                {
+                    statChange = maxStatVal;
+                }
+                // string, float
+                gremlin.setStat(stat, statChange);
+                gremlin.setStat("Happiness", 1 + gremlin.getStat("Happiness"));
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+        //Debug.Log(other.tag);
+        //Debug.Log("hello");
+        if (other.tag == "Gremlin")
+        {
+            beingEaten = true;
+            gremlin = other.GetComponent<GremlinObject>().gremlin;
+            AudioSource gremlinSound = other.gameObject.GetComponent<AudioSource>();
+            gremlinSound.Play();
+        }
+    }
+
+    private string determineStat(string food)
+    {
+        string stat = "";
+        switch (food)
+        {
+            case "Apple":
+                stat = "Stamina";
+                break;
+            case "Cheetah Fruit":
+                stat = "Running";
+                break;
+            case "Monkey Fruit":
+                stat = "Climbing";
+                break;
+            case "Dolphin Fruit":
+                stat = "Swimming";
+                break;
+            case "Dragon Fruit":
+                stat = "Flying";
+                break;
+            default:
+                break;
+        }
+        return stat;
+    }
+}
---
+++ Gremlin Gardens/Assets/Scripts/PathToFood.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathToFood : MonoBehaviour
+{
+    public float viewRadius;
+    public float viewDistance = 50.0f;
+    public int time1 = 4;
+
+    //private LineRenderer laserLine;
+    private RaycastHit hit;
+    //private beingCarried = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //RaycastHit hit;
+
+        // Get and store references to line renderer component
+        // laserLine = GetComponent<LineRenderer>();
+        StartCoroutine(timer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        //Debug.DrawRay(transform.position, forward, Color.green);
+        // Can use an invisible cone instead to simulate sight
+        if (Physics.Raycast(transform.position, forward, out hit, viewDistance))
+        {
+            if (hit.transform.tag == "Fruit")
+            {
+                //Debug.Log(hit.distance);
+                //Debug.Log("fruit");
+                //Debug.Log(hit.transform.position);
+            }
+        }
+    }
+
+    IEnumerator timer()
+    {
+        while (true)
+        {
+            //Debug.Log("timer");
+            yield return new WaitForSeconds(time1);
+        }
+    }
+}
