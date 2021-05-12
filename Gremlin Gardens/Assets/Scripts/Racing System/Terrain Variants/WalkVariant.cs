85d00526c5fcc17a56fd78974db301d1695e8726
ambiguousname <tyly04@gmail.com>
2021-05-12 13:33:05 -0700

    First race now has prefabs

--- Gremlin Gardens/Assets/Scripts/Racing System/Terrain Variants/WalkVariant.cs
+++ Gremlin Gardens/Assets/Scripts/Racing System/Terrain Variants/WalkVariant.cs
@@ -8,8 +8,4 @@ public class WalkVariant : TerrainVariant
     public override float relativeSpeed(GremlinObject gremlin, TrackModule activeModule) {
         return Mathf.Log(2 + gremlin.gremlin.getStat("Running"), 100) * speedModifier;
     }
-
-    public override Vector3 positionFunction(float time, TrackModule activeModule) {
-        return new Vector3(0, 0, Mathf.Sin(time * 15)/5);
-    }
 }
