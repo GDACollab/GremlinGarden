ea56482915a136b605fb6b57b77d879dbca3441f
Joshua <jaugees@gmail.com>
2021-05-15 13:15:22 -0700

    Fixed racing music and buying items with no money

--- Gremlin Gardens/Assets/Scripts/Shop/ShopItem.cs
+++ Gremlin Gardens/Assets/Scripts/Shop/ShopItem.cs
@@ -56,20 +56,21 @@ public class ShopItem : MonoBehaviour
         if (Vector3.Distance(manager.player.transform.position, this.transform.position) < buyDistance && mouseOn)
         {
             // enableMovement is to make sure the player is not in a menu or something when clicking
-            if (Input.GetKeyDown(KeyCode.Mouse0) && purchaseIntent == false && manager.player.GetComponent<PlayerMovement>().enableMovement)
+            PlayerMovement playerMovement = manager.player.GetComponent<PlayerMovement>();
+            if (Input.GetKeyDown(KeyCode.Mouse0) && purchaseIntent == false && playerMovement.enableMovement && playerMovement.GetMoney() > 0)
             {
                 purchaseIntent = true;
                 manager.SetPurchaseText("Confirm Buy " + itemName + "?");
-            } else if (Input.GetKeyDown(KeyCode.Mouse0) && purchaseIntent == true && manager.player.GetComponent<PlayerMovement>().enableMovement) {
+            } else if (Input.GetKeyDown(KeyCode.Mouse0) && purchaseIntent == true && playerMovement.enableMovement) {
                 purchaseIntent = false;
                 // Quick hack to detect whether or not we're spawning a gremlin.
                 if (itemSpawnOnBuy.TryGetComponent<GremlinObject>(out GremlinObject gremlin)){
                     // Quick hack to find the GameManager:
-                    manager.player.GetComponent<PlayerMovement>().UpdateMoney(-500);
+                    playerMovement.UpdateMoney(-500);
                     GameObject.Find("GameManager").GetComponent<GremlinSpawner>().CreateGremlin(manager.player.transform.position + manager.player.transform.forward);
                 } else {
                     var bought = Instantiate(itemSpawnOnBuy);
-                    manager.player.GetComponent<PlayerMovement>().UpdateMoney(-100);
+                    playerMovement.UpdateMoney(-100);
                     //Temporary solution for placement.
                     bought.transform.position = manager.player.transform.position + manager.player.transform.forward;
                 }
