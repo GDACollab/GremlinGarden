8c59c9b9b43bea25cc766e3779cb2b462654e38e
rylee12 <ryanlee7x@gmail.com>
2021-05-14 18:29:23 -0700

    Update: Gremlin Movement
    It works, but vision is raycast, which is narrow. Gremlin speed and smoothness needs to be hashed out.

--- Gremlin Gardens/Assets/Scripts/MoveGremlin.cs
+++ Gremlin Gardens/Assets/Scripts/MoveGremlin.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class MoveGremlin : MonoBehaviour
 {
-    private Vector3 vect1 = new Vector3(23.3f, 34.9f, 40.9f);
     public float speed = 1.0f;
+
     private PathToFood path_script;
-    private Vector3[] list3 = new Vector3[50];
+    //private Vector3[] list3 = new Vector3[50];
+    private Vector3 dest;
+    private int length;
+
+    // Debugging variables
+    //private Vector3 vect1 = new Vector3(23.3f, 34.9f, 40.9f);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +28,16 @@ public class MoveGremlin : MonoBehaviour
         //transform.position = Vector3.MoveTowards(transform.position, vect1, step);
         if (path_script.move_to_ == true)
         {
-            list3[0] = path_script.path_.corners[0];
-            Vector3 pos = Vector3.MoveTowards(transform.position, list3[0], 10.0f * Time.deltaTime);
+            //list3[0] = path_script.path_.corners[0];
+            length = path_script.path_.corners.Length - 1;
+            dest = new Vector3(path_script.path_.corners[length].x, path_script.path_.corners[length].y, path_script.path_.corners[length].z);
+
+            //Vector3 pos
+            this.transform.position = Vector3.MoveTowards(transform.position, dest, 0.1f);
+
+            //Debug.LogWarning(pos);
+            //this.transform.position = Vector3.Lerp(pos, this.transform.position, Time.deltaTime);
+            //Debug.Log(path_script.path_.corners[0].x);
         }
     }
 }
--- Gremlin Gardens/Assets/Scripts/PathToFood.cs
+++ Gremlin Gardens/Assets/Scripts/PathToFood.cs
@@ -5,23 +5,27 @@ using UnityEngine.AI;
 
 public class PathToFood : MonoBehaviour
 {
-    private Transform target;
-    private NavMeshPath path;
-    private float elapsed = 0.0f;
+    // view distance of gremlin for raycast
     public float viewDistance = 50.0f;
+    // determines how frequently the gremlin moves (not smooth)
+    public float endtime = 0.3f;
+
+    private float elapsed = 0.0f;
+    // determines whether to move gremlin or not
+    private bool move_to = false;
 
     private RaycastHit hit;
-    private bool move_to = false;
-    private Vector3[] list3 = new Vector3[50];
+    private Transform target;
+    private NavMeshPath path;
+    private NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
-        //RaycastHit hit;
         path = new NavMeshPath();
         elapsed = 0.0f;
-        //Debug.Log(target);
-        //Debug.Log(this.gameObject);
+        // agent necessary? Make sure to re-examine
+        agent = this.GetComponent<NavMeshAgent>();
 
         //StartCoroutine(timer());
     }
@@ -39,30 +43,34 @@ public class PathToFood : MonoBehaviour
             }
         }
 
+        // Debug test purposes
+        //target = GameObject.FindGameObjectsWithTag("Fruit")[0].transform;
+        // 2 problems:
+        // Gremlin can go through objects
+        // Gremlin can move to object even if it is being carried
+
         elapsed += Time.deltaTime;
-        if (elapsed > 1.0f && target != null)
+        if (elapsed > endtime && target != null)
         {
-            elapsed -= 1.0f;
+            elapsed -= endtime;
             NavMesh.CalculatePath(transform.position, target.position, UnityEngine.AI.NavMesh.AllAreas, path);
             move_to = true;
-            //UnityEngine.AI.NavMeshAgent.SetPath(path);
         }
         else
         {
             move_to = false;
+            agent.ResetPath();
         }
 
         // debugging path
         for (int i = 0; i < path.corners.Length - 1; i++)
         {
-            Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
-            //Debug.Log(path.corners[i]);
-            Debug.Log(path.corners.Length - 1);
-            list3[i] = path.corners[i];
-            Debug.Log(list3[i]);
+            //Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
+            //Debug.Log(path.corners.Length - 1);
+            //list3[i] = path.corners[i];
+            //Debug.Log(list3[i]);
         }
 
-        //transform.position = Vector3.MoveTowards(transform.position, list3[0], 2.0f * Time.deltaTime);
     }
 
     public NavMeshPath path_
