c974d4c15ba818bb67334c41e537752c796562f0
Joshua <jaugees@gmail.com>
2021-05-26 16:11:04 -0700

    Update StuckObj.cs

--- Gremlin Gardens/Assets/Scripts/Misc/StuckObj.cs
+++ Gremlin Gardens/Assets/Scripts/Misc/StuckObj.cs
@@ -11,5 +11,8 @@ public class StuckObj : MonoBehaviour
 
     void OnTriggerEnter(Collider other){
         other.gameObject.transform.position = player.transform.position + new Vector3(0, 3, 0);
+        // while(Physics.Raycast(other.gameObject.transform.position, Vector3.up, 100)){
+        //     other.gameObject.transform.position = new Vector3(other.gameObject.transform.position.x, other.gameObject.transform.position.y + 5.0f, other.gameObject.transform.position.z);
+        // }
     }
 }
