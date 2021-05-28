3ed87c4f27fcf5a15c31e446194b6e777607aed4
rylee12 <ryanlee7x@gmail.com>
2021-05-27 21:19:05 -0700

    Shop Assets
    Shop items look like their object instead of a sphere

---
+++ Gremlin Gardens/Assets/Scripts/MeshCombine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCombine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshFilter[] filters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[filters.Length];
+
+        for (int i = 0; i < filters.Length; i++)
+        {
+            combine[i].mesh = filters[i].sharedMesh;
+            combine[i].transform = filters[i].transform.localToWorldMatrix;
+            filters[i].gameObject.SetActive(false);
+
+        }
+        transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        transform.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
