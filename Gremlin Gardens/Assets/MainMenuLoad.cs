bffa3780ea0a401ed4c266421c02219f99a784b1
Thomas Applewhite <thomashapplewhite@gmail.com>
2021-05-29 11:05:50 -0700

    Unanchored the cursor every time the main menu is loaded

---
+++ Gremlin Gardens/Assets/MainMenuLoad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the cursor to be in freelook, if it isn't already
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+}
